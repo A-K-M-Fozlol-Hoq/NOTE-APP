82399f3778040770fc0e9926900c8178348c0031
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-07-04 04:09:04 +0600

    signin component on progress

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/home';
 import Signin from './src/screens/signin';
@@ -8,11 +8,19 @@ import Signup from './src/screens/signup';
 import Edit from './src/screens/edit';
 import Create from './src/screens/create';
 
+const appTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#fff',
+  },
+};
+
 const Stack = createNativeStackNavigator();
 export default function App() {
   const user = false; // not authenticated
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={appTheme}>
       <Stack.Navigator>
         {user ? (
           <>
@@ -22,7 +30,11 @@ export default function App() {
           </>
         ) : (
           <>
-            <Stack.Screen name="Signin" component={Signin} />
+            <Stack.Screen
+              name="Signin"
+              component={Signin}
+              options={{ headerShown: false }}
+            />
             <Stack.Screen name="Signup" component={Signup} />
           </>
         )}
--- src/screens/signin.jsx
+++ src/screens/signin.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, SafeAreaView,Image,  Text, TextInput } from 'react-native'
 import React from 'react'
 
 export default function Signin() {
   return (
-    <View>
-      <Text>Signin</Text>
-    </View>
+    <SafeAreaView>
+      <Image
+        style={{ height: 270, width: 320,  alignSelf: 'center' }}
+        source={require('../../assets/empty-state.png')}
+      />
+      <Text style={{fontSize:18, fontWeight: 'bold', textAlign: 'center'}}>Never forget your notes</Text>
+      <View>
+        <TextInput placeholder="emaol" />
+      </View>
+    </SafeAreaView>
   )
 }
