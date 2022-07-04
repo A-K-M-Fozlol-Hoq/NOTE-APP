b1744528720304fd60c7bc0838c4994e714476e8
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-07-05 01:53:38 +0600

    signup component UI complete

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,9 +1,11 @@
 import { View, SafeAreaView,Image,  Text, TextInput,StyleSheet, Pressable,  } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../components/Button'
 import Input from '../components/Input'
 
 export default function Signup({navigation}) {
+  const genderOptions = ["Male", "Female"]
+  const [gender, setGender] = useState(null)
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{paddingHorizontal:16, paddingVertical:25}}>
@@ -12,12 +14,19 @@ export default function Signup({navigation}) {
         <Input placeholder="Full name" />
         <Input placeholder="Age" />
 
-        <Pressable stye={styles.radioContainer}>
-          <View style={styles.outerCircle}>
-            <View style={styles.innerCircle}></View>
-          </View>
-          <Text style={styles.radioText}>Male</Text>
-        </Pressable>
+        {
+          genderOptions.map(option =>{
+            const selected = option === gender;
+            return (
+              <Pressable onPress={()=>setGender(option)} key={option} style={styles.radioContainer}>
+              <View style={[styles.outerCircle, selected && styles.selectedOuterCircle]}>
+                <View style={[styles.innerCircle, selected && styles.selectedInnerCircle]}></View>
+              </View>
+              <Text style={styles.radioText}>{option}</Text>
+            </Pressable>
+            )
+          })
+        }
 
       </View>
 
@@ -32,11 +41,34 @@ export default function Signup({navigation}) {
 }
 const styles=StyleSheet.create({
   radioContainer:{
+    flexDirection:"row",
+    alignItems: "center",
+    marginBottom:25,
   },
   outerCircle:{
+    height:30,
+    width:30,
+    borderRadius:15,
+    borderWidth:1,
+    borderColor: '#cfcfcf',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   innerCircle:{
+    height:15,
+    width:15,
+    borderRadius:7.5,
+    borderWidth:1,
+    borderColor: '#cfcfcf',
   },
   radioText:{
+    marginLeft:10
   },
+  selectedOuterCircle:{
+    borderColor:"orange",
+  },
+  selectedInnerCircle:{
+    backgroundColor:"orange",
+    borderColor:"orange"
+  }
 })
