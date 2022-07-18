dc1d1c5df9a068aa1c30c20d1609e8e6dec1f87f
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-07-19 03:14:03 +0600

    added icon, have to implement create note

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { AntDesign } from '@expo/vector-icons';
 
-export default function Home() {
+export default function Home({navigation, route, user}) {
+  console.log("user --> ", user)
+  const onPressCreate =()=>{
+    navigation.navigate("create")
+  }
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flexDirection:'row',justifyContent: 'space-between', padding:20}}>
         <Text>My Notes</Text>
+        <Pressable onPress={onPressCreate}><AntDesign name="pluscircleo" size={24} color="black" /></Pressable>
+        
       </View>
     </SafeAreaView>
   )
