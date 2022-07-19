1ab095349d5e42d2c38e847d0f57e64ffbce4871
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-07-19 20:25:54 +0600

    fetched notes from firebase/firestore

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,10 +1,28 @@
 import { View, Text, Pressable } from 'react-native'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { AntDesign } from '@expo/vector-icons';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import { db } from '../../App';
 
 export default function Home({navigation, route, user}) {
-  console.log("user --> ", user)
+  // console.log("user --> ", user)
+  const [notes, setNotes] = useState([]);
+
+  useEffect(()=>{
+    const q = query(collection(db , 'notes'), where("uid" , "==", user.uid))
+    const notesListenerSubscription = onSnapshot(q, (querySnapshot) =>{
+      const list =[];
+      querySnapshot.forEach((doc)=>{
+        list.push(doc.data());
+      })
+      setNotes(list);
+    })
+    return notesListenerSubscription;
+  },[])
+
+  console.log(notes)
+
   const onPressCreate =()=>{
     navigation.navigate("Create")
   }
